ead3dd221d36173702676dd83c4a744b060144da
Danny <thanhan1100@gmail.com>
2024-09-19 15:34:35 +0700

    Add project files.

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Store_Manager.Models;
+
+namespace Store_Manager.Controllers
+{
+    public class ProductController : Controller
+    {
+        static List<Product> products = new List<Product>();
+
+        public IActionResult Index()
+        {
+            ViewData["Heading"] = "All Product";
+            var products = new List<Product>();
+            products.Add(new Product { Id = 101, Price = 99000, ProductName = "IOS" });
+            products.Add(new Product { Id = 102, Price = 99000, ProductName = "Android" });
+            products.Add(new Product { Id = 103, Price = 99000, ProductName = "Windows" });
+
+            return View(products);
+        }
+
+
+    }
+}
