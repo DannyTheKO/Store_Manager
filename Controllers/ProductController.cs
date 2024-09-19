8f5cdc6b2957ea092384e5a500114ae3742c57f4
Danny <thanhan1100@gmail.com>
2024-09-19 22:26:38 +0700

    Big Update

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -7,27 +7,31 @@ namespace Store_Manager.Controllers
     {
         static List<Product> products = new List<Product>();
 
+
         public IActionResult Index()
         {
 
-            return View();
+            return Redirect("Product/ShowAll");
+        }
+        public IActionResult ShowAll()
+        {
+            return View("ShowAll", products);
         }
 
         // Starting DB
-        public IActionResult ShowAll()
-        {
-            ViewData["Heading"] = "Product Management";
+        //public IActionResult ShowAll()
+        //{
+        //    ViewData["Heading"] = "Product Management";
          
-            var products = new List<Product>();
-            products.Add(new Product { Id = 101, Price = 99000, ProductName = "IOS" });
-            products.Add(new Product { Id = 102, Price = 99000, ProductName = "Android" });
-            products.Add(new Product { Id = 103, Price = 99000, ProductName = "Windows" });
+        //    var products = new List<Product>();
+        //    products.Add(new Product { Id = 101, Price = 99000, ProductName = "IOS" });
+        //    products.Add(new Product { Id = 102, Price = 99000, ProductName = "Android" });
+        //    products.Add(new Product { Id = 103, Price = 99000, ProductName = "Windows" });
 
-            return View(products);
-        }
+        //    return View(products);
+        //}
 
         // Create
-
         public IActionResult Create()
         {
             return View("Create");
@@ -40,6 +44,39 @@ namespace Store_Manager.Controllers
             return RedirectToAction("ShowAll");
         }
 
+        // Edit
+        public IActionResult Edit(int id)
+        {
+            Product p = products.SingleOrDefault(q => q.Id == id);
+            if (p != null) // Tìm Thấy
+                return View(p);
+            else
+                return NotFound();
+        }
+
+        [HttpPost]
+        public IActionResult Edit(int id, [Bind("Id", "ProductName", "Price")] Product product)
+        {
+            Product p = products.SingleOrDefault(q => q.Id == id);
+            if (p != null) // Tìm Thấy
+            {
+                p.ProductName = product.ProductName;
+                p.Price = product.Price;
+            }
+
+            return RedirectToAction("ShowAll");
+        }
+
+        public IActionResult Delete(int id)
+        {
+            Product p = products.SingleOrDefault(q => q.Id == id);
+            if (p != null) // Found
+            {
+                products.Remove(p);
+            }
+
+            return RedirectToAction("ShowAll");
+        }
 
     }
 }
